60d6dcf14436171e4581d35fc297270cca52b41c
lawl <root@s3cr3t.de>
2013-04-28 00:23:05 +0200

    actually implement playlist support

--- js/replace.js
+++ js/replace.js
@@ -1,14 +1,35 @@
 window.onload=function(){
         var all = document.getElementsByClassName('klang');
         for(var i = 0; i < all.length; i++) { 
-            all[i].childNodes[0].setAttribute('onClick', 'playTheShit(' + i + ')'); 
+            all[i].childNodes[0].setAttribute('onClick', 'getAudioById(' + i + ').play()'); 
 
             all[i].childNodes[0].className += " taster";
             all[i].childNodes[2].style.display = 'none'; 
         }
+        playlist();
 }
 
-function playTheShit(id)
-{
-    document.getElementsByClassName("klang")[id].childNodes[2].play();
+function playlist(){
+	var hashes=document.location.hash.substr(1);
+	if(typeof hashes !== 'undefined' && hashes.length > 1) {
+		var els=document.getElementsByTagName('h2');
+		var hashlist=hashes.split(',');
+		var duration=0;
+		for(var chash in hashlist){
+			var hash=hashlist[chash];
+			for(var i in els) {
+				var name = els[i].innerHTML +'';
+				if((name.replace(/ /g, '').toLowerCase()).indexOf(hash) !== -1){
+					setTimeout(function(i){getAudioById(i).play();},duration*1000,i);
+					duration+=getAudioById(i).duration;;	
+					break;
+				}
+			}
+		}
+	}
+	window.onhashchange=playlist;
+}
+
+function getAudioById(id){
+	return document.getElementsByClassName("klang")[id].childNodes[2];
 }
